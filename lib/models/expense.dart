d6ef9d1c684c87b7ec0a8bfba3157a311102ed46
LT-EDASTAN <edastan@limak.com.tr>
2023-09-14 17:14:36 +0300

    .

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -1,11 +1,34 @@
-
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:uuid/uuid.dart';
 
-final uuid = Uuid();
+final formatter = DateFormat.yMd();
+const uuid = Uuid();
+
+enum Category { shopping, investment, foreignCurrency, gold }
+
+const categoryIcons = {
+  Category.shopping: Icons.shopify,
+  Category.investment: Icons.bar_chart,
+  Category.foreignCurrency: Icons.attach_money,
+  Category.gold: Icons.money,
+};
+
 class Expense {
-  Expense({required this.title, required this.amount, required this.date}): id = uuid.v4();
+  Expense(
+      {required this.title,
+      required this.amount,
+      required this.date,
+      required this.cataegory})
+      : id = uuid.v4();
   final String id;
   final String title;
   final double amount;
   final DateTime date;
+  final Category cataegory;
+
+  String get formattedDate {
+    return formatter.format(date);
+  }
 }
---
+++ lib/widgets/new_expense.dart
@@ -0,0 +1,147 @@
+import 'package:expense_tracker_app/models/expense.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+final formatter = DateFormat.yMd();
+
+class NewExpense extends StatefulWidget {
+  const NewExpense({super.key});
+  @override
+  State<StatefulWidget> createState() {
+    return _NewExpenseState();
+  }
+}
+
+class _NewExpenseState extends State<NewExpense> {
+  final _titleController = TextEditingController();
+  final _amountController = TextEditingController();
+  DateTime? _selectedDate;
+  Category _selectedCategory = Category.shopping;
+  void _presentDatePicker() async {
+    final now = DateTime.now();
+    final firstDate = DateTime(now.day, now.month, now.year - 1);
+
+    final pickedDate = await showDatePicker(
+        context: context,
+        initialDate: now,
+        firstDate: firstDate,
+        lastDate: now);
+
+    setState(() {
+      _selectedDate = pickedDate;
+    });
+  }
+
+  void _submitExpenseData() {
+    final enteredAmount = double.tryParse(_amountController.text);
+    final amountIsInvalid = enteredAmount == null || enteredAmount <= 0;
+    if (_titleController.text.trim().isEmpty ||
+        amountIsInvalid ||
+        _selectedDate == null) {
+      showDialog(
+        context: context,
+        builder: (ctx) => AlertDialog(
+          title: const Text('Geçersiz girdi'),
+          content: const Text("Lütfen geçerli girdiler giriniz"),
+          actions: [
+            TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: const Text('Tamam'))
+          ],
+        ),
+      );
+      return;
+    }
+  }
+
+  @override
+  void dispose() {
+    _titleController.dispose();
+    _amountController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16),
+      child: Column(
+        children: [
+          TextField(
+            controller: _titleController,
+            maxLength: 50,
+            decoration: InputDecoration(
+              label: Text('Başlık'),
+            ),
+          ),
+          Row(
+            children: [
+              Expanded(
+                child: TextField(
+                  controller: _amountController,
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                    prefixText: '\₺',
+                    label: Text('Miktar'),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                width: 16,
+              ),
+              Expanded(
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Text(_selectedDate == null
+                        ? "Tarih Seçiniz"
+                        : formatter.format(_selectedDate!)),
+                    IconButton(
+                        onPressed: _presentDatePicker,
+                        icon: const Icon(Icons.calendar_month))
+                  ],
+                ),
+              )
+            ],
+          ),
+          const SizedBox(height: 16),
+          Row(
+            children: [
+              DropdownButton(
+                  value: _selectedCategory,
+                  items: Category.values
+                      .map(
+                        (category) => DropdownMenuItem(
+                          value: category,
+                          child: Text(
+                            category.name.toUpperCase(),
+                          ),
+                        ),
+                      )
+                      .toList(),
+                  onChanged: (value) {
+                    setState(() {
+                      if (value == null) {
+                        return;
+                      }
+                      _selectedCategory = value;
+                    });
+                  }),
+              const Spacer(),
+              TextButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: const Text('Çık')),
+              ElevatedButton(
+                  onPressed: _submitExpenseData, child: const Text("Kaydet"))
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
